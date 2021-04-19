1188cd2b073e396b017b846b8ba56ea2ceb03d57
yangjoanne216 <63150365+yangjoanne216@users.noreply.github.com>
2021-04-19 18:11:29 +0200

    changer la fonction getDiff

--- getmodification-archeolex.py
+++ getmodification-archeolex.py
@@ -1,6 +1,7 @@
 import os
 import re
-import git 
+import git
+import difflib 
 from git.compat import defenc
 
 def createRepo(*codes_list):
@@ -21,31 +22,28 @@ def createRepo(*codes_list):
 
 def getDiff(type_code,number_commit):
     path='archeo_lex/'+type_code
-    repo = Repo.clone_from(url='https://archeo-lex.fr/codes/'+type_code,to_path=path)
+    repo = git.Repo(path)
     os.chdir(path)
-    #Parcourir tous les commits et obtenir le commit(hcommit) dont hexsha est type_code
-    commits = list(repo.iter_commits('texte'))
-    for i in commits:
-        if i.hexsha == number_commit:
-            hcommit = i
-
+    commit = repo.commit(number_commit)
     #Obtenit le diff selon le commit(le diff de le commit que l'on a entré et le commit précédent)
-    diff = hcommit.diff(hcommit.parents[0], create_patch=True)
-    print(hcommit.parents[0].hexsha)
-    for k in diff:
-        msg = k.diff.decode(defenc)
-        #print(msg) #on peut print tous les information de diff
-        
-        #On obtient l'informations nécessaire pour csv 
-        print("Code Version Date modifier_location Partie Nature Sous-partie Livre Titre Article Type")
-
-        results = re.findall(".*@@(.*)@@.*",str(msg))  # @@****@@ est le début d'une modification(le début de chunck)
-        for match in results:
-            print(type_code,end=" ") 
-            print(hcommit.hexsha,end=" ")
-            print(hcommit.committed_date,end=" ")
-            print(match)
-
+    diff = commit.diff(commit.parents[0], create_patch=True).pop()
+    print(commit.parents[0])
+    a=diff.a_blob.data_stream.read().decode('utf-8')
+    b=diff.b_blob.data_stream.read().decode('utf-8').splitlines()
+    #differ = difflib.Differ()
+    #lines = list(differ.compare(b,a))
+    print(a)
+    """
+    for line in lines:
+        print(line)
+    """
+    '''
+    for num,txt in enumerate(lines):
+        if txt.startswith('  #######'):
+            print(num,':',txt)
+    '''
+    
+    
 '''
 quelques versions de commit pour code_civil
 2b5d875bf431d8cf21df3c376551d7b7802e7a75
@@ -55,14 +53,7 @@ f1975506ff1acf167891aa2287e88f5330f2bab3
 '''
 
 #main
-#getDiff("code_civil","2b5d875bf431d8cf21df3c376551d7b7802e7a75")
-createRepo('code_civil','code_de_la_propriété_intellectuelle')
+getDiff("code_civil","f1975506ff1acf167891aa2287e88f5330f2bab3")
+#createRepo('code_civil','code_de_la_propriété_intellectuelle')
 
 
-'''
-#Récupérer tous les codes de archeo-lex
-def getAllTheRepo():
-    codes = ['code_civil','code_de_la_propriété_intellectuelle','code_de_l\'éducation']
-    for code in codes:
-        repo = Repo.clone_from(url='https://archeo-lex.fr/codes/'+code,to_path='../'+code)
-''' 
